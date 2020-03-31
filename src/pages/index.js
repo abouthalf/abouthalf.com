540fb115d8f11ac8357c6d3260427a118c960174
Michael Barrett <mb@abouthalf.com>
2020-03-30 18:25:23 -0700

    Generated index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,164 +1,85 @@
+import fs from "fs";
+import path from "path";
+import Airtable from "airtable";
 import SocialHeadMeta from "../components/SocialHeadMeta";
 import MegaGram from "../components/MegaGram";
 
-import MegaGram20200329 from "../public/images/2020/03/29/index";
-import MegaGram20200301 from "../public/images/2020/03/01/index";
-import MegaGram20200224 from "../public/images/2020/02/24/index";
-import MegaGram20200223 from "../public/images/2020/02/23/index";
-import MegaGram20200201 from "../public/images/2020/02/01/index";
-import MegaGram20200120 from "../public/images/2020/01/20/index";
-import MegaGram20191214 from "../public/images/2019/12/14/index";
-import MegaGram20191208 from "../public/images/2019/12/08/index";
-import MegaGram20191207 from "../public/images/2019/12/07/index";
-import MegaGram20191114 from "../public/images/2019/11/14/index";
-import MegaGram20191110 from "../public/images/2019/11/10/index";
-import MegaGram20190104 from "../public/images/2019/01/04/index";
-import MegaGram20181028 from "../public/images/2018/10/28/index";
-import MegaGram20181020 from "../public/images/2018/10/20/index";
-import MegaGram20181017 from "../public/images/2018/10/17/index";
-import MegaGram20180908 from "../public/images/2018/09/08/index";
-import MegaGram20180821 from "../public/images/2018/08/21/index";
-import MegaGram20180809 from "../public/images/2018/08/09/index";
-import MegaGram20180801 from "../public/images/2018/08/01/index";
-import MegaGram20180715 from "../public/images/2018/07/15/index";
-import MegaGram20180708 from "../public/images/2018/07/08/index";
-import MegaGram20180624 from "../public/images/2018/06/24/index";
+export default ({ posts }) => {
+    const renderPosts = posts => {
+        return posts.map(post => {
+            const { date, year, month, day, title, index } = post;
+            const d = new Date(date);
+            const formattedDate = d.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+            });
+            const path = `/images/${year}/${month}/${day}/`;
+            return (
+                <MegaGram
+                    key={date}
+                    index={index}
+                    title={title}
+                    date={formattedDate}
+                    path={path}
+                    year={year}
+                    month={month}
+                    day={day}
+                />
+            );
+        });
+    };
 
-export default () => (
-    <article>
-        <SocialHeadMeta />
+    return (
+        <article>
+            <SocialHeadMeta />
+            {renderPosts(posts)}
+        </article>
+    );
+};
 
-        <MegaGram
-            index={MegaGram20200329}
-            title="When life gives you lemons make concrete 🍋"
-            path="/images/2020/03/29/"
-            date="March 29 2020"
-        />
-        <MegaGram
-            index={MegaGram20200301}
-            title="Recapitulated with squares"
-            path="/images/2020/03/01/"
-            date="March 1 2020"
-        />
-        <MegaGram
-            index={MegaGram20200224}
-            title="Bundle up, it’s raining"
-            path="/images/2020/02/24/"
-            date="February 24 2020"
-        />
-        <MegaGram
-            index={MegaGram20200223}
-            title="Cold front followed by tropical thought balloons. "
-            path="/images/2020/02/23/"
-            date="February 23 2020"
-        />
-        <MegaGram
-            index={MegaGram20200201}
-            title="Finally some sun in Portland"
-            path="/images/2020/02/01/"
-            date="February 1 2020"
-        />
-        <MegaGram
-            index={MegaGram20200120}
-            title="Overlapping idealizations."
-            path="/images/2020/01/20/"
-            date="January 20 2020"
-        />
-        <MegaGram
-            index={MegaGram20191214}
-            title="Sweater weather. Portrait study from a Uniqlo advertisement."
-            path="/images/2019/12/14/"
-            date="December 14 2019"
-        />
-        <MegaGram
-            index={MegaGram20191208}
-            title="Recent work. Three printer paintings. Inkjet on Canvas with Acrylic, mounted on birch panel."
-            path="/images/2019/12/08/"
-            date="December 8 2019"
-        />
-        <MegaGram
-            index={MegaGram20191207}
-            title="Late night, walking the dog"
-            path="/images/2019/12/07/"
-            date="December 7 2019"
-        />
-        <MegaGram
-            index={MegaGram20191114}
-            title="Trains, trees, windswept"
-            path="/images/2019/11/14/"
-            date="November 14 2019"
-        />
-        <MegaGram
-            index={MegaGram20191110}
-            title="Eggs over Brighton"
-            path="/images/2019/11/10/"
-            date="November 10 2019"
-        />
-        <MegaGram
-            index={MegaGram20190104}
-            title="Boy looking over shoulder"
-            path="/images/2019/01/04/"
-            date="January 4 2019"
-        />
-        <MegaGram
-            index={MegaGram20181028}
-            title="Portrait study with eggs"
-            path="/images/2018/10/28/"
-            date="October 28, 2018"
-        />
-        <MegaGram
-            index={MegaGram20181020}
-            title="Autumn landed with a thunk"
-            path="/images/2018/10/20/"
-            date="October 20, 2018"
-        />
-        <MegaGram
-            index={MegaGram20181017}
-            title="Waiting for a drink. Waiting at the airport. Waiting waiting waiting."
-            path="/images/2018/10/17/"
-            date="October 17, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180908}
-            title="Ghosts in the walls of an old house in Portland."
-            path="/images/2018/09/08/"
-            date="September 8, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180821}
-            title="Sunday sunset train to Portland. Wednesday sunrise train to Seattle. Notes and scribbles. Painted sunset over Tacoma. Shadowy figures."
-            path="/images/2018/08/21/"
-            date="August 21, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180809}
-            title="Wednesday morning commute. Bright, sunny, warm, and humid."
-            path="/images/2018/08/09/"
-            date="August 9, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180801}
-            title="Avocados in space over a no-tell motel."
-            path="/images/2018/08/01/"
-            date="August 1, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180715}
-            title="Poppies and polka-dots."
-            path="/images/2018/07/15/"
-            date="July 15, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180708}
-            title="Triple process portrait with orange poppies."
-            path="/images/2018/07/08/"
-            date="July 8, 2018"
-        />
-        <MegaGram
-            index={MegaGram20180624}
-            title="For today's active man or woman"
-            path="/images/2018/06/24/"
-            date="June 24, 2018"
-        />
-    </article>
-);
+export async function getStaticProps() {
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+        "app7EYyBtFZSDnzUQ",
+    );
+    const records = await base("Posts")
+        .select({
+            maxRecords: 100,
+            view: "Grid view",
+            sort: [{ field: "Date", direction: "desc" }],
+        })
+        .firstPage();
+    const posts = records.map(post => {
+        const {
+            Title: title,
+            Description: description = "",
+            Keywords: keywords = "",
+            Date: date,
+            Type: type,
+        } = post.fields;
+        const [year, month, day] = date.split("-");
+        const idxPath = path.join(
+            process.cwd(),
+            `src/public/images/${year}/${month}/${day}/index.json`,
+        );
+        const idxContents = fs.readFileSync(idxPath);
+        const index = JSON.parse(idxContents);
+        return {
+            title,
+            description,
+            keywords,
+            date,
+            year,
+            month,
+            day,
+            type,
+            index,
+        };
+    });
+    console.log(posts);
+    return {
+        props: {
+            posts,
+        },
+    };
+}
