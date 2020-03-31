692261a7f18ea005bbf3c0cacab45f0c2bf37634
Michael Barrett <mb@abouthalf.com>
2020-03-30 17:04:35 -0700

    Working post page

--- src/pages/p/[year]/[month]/[day]/index.js
+++ src/pages/p/[year]/[month]/[day]/index.js
@@ -1,31 +1,59 @@
-import { useRouter } from "next/router";
-// import dynamic from "next/dynamic";
+import Airtable from "airtable";
+
 import SocialHeadMeta from "../../../../../components/SocialHeadMeta";
 import MegaGram from "../../../../../components/MegaGram";
 
 import fs from "fs";
 import path from "path";
 
-export default ({ year, month, day, idx }) => {
-    // const router = useRouter();
-    // const { year, month, day } = router.query;
-
-    const d = new Date(year, month, day);
+export default ({
+    title,
+    keywords,
+    description,
+    date,
+    type,
+    year,
+    month,
+    day,
+    idx,
+}) => {
+    const d = new Date(date);
     const formattedDate = d.toLocaleDateString("en-US", {
         year: "numeric",
         month: "long",
         day: "numeric",
     });
 
+    // collect all props for dynamically selected component
+    const componentProps = {
+        title,
+        keywords,
+        description,
+        date,
+        type,
+        year,
+        month,
+        day,
+        index: idx,
+        path: `/images/${year}/${month}/${day}/`,
+        formattedDate,
+    };
+
+    const renderComponent = props => {
+        if (type === "MegaGram") {
+            return <MegaGram {...props} />;
+        }
+        return null;
+    };
+
     return (
         <article>
-            <SocialHeadMeta />
-            <MegaGram
-                index={idx}
-                title="title"
-                path={`/images/${year}/${month}/${day}/`}
-                date={formattedDate}
+            <SocialHeadMeta
+                title={title}
+                keywords={keywords}
+                description={description}
             />
+            {renderComponent(componentProps)}
         </article>
     );
 };
@@ -40,19 +68,64 @@ export async function getStaticProps({ params }) {
     const idxContents = fs.readFileSync(idxPath);
     const idx = JSON.parse(idxContents);
 
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+        "app7EYyBtFZSDnzUQ",
+    );
+    const posts = await base("Posts")
+        .select({
+            maxRecords: 1,
+            view: "Grid view",
+            filterByFormula: `DATESTR({Date}) = "${year}-${month}-${day}"`,
+        })
+        .firstPage();
+
+    const { fields } = posts[0];
+    const {
+        Title: title,
+        Description: description,
+        Keywords: keywords,
+        Date: date,
+        Type: type,
+    } = fields;
     return {
         props: {
-            year,
-            month,
-            day,
+            ...params,
+            title,
+            description,
+            keywords,
+            date,
+            type,
             idx,
         },
     };
 }
 
 export async function getStaticPaths() {
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+        "app7EYyBtFZSDnzUQ",
+    );
+    const posts = await base("Posts")
+        .select({
+            maxRecords: 100,
+            view: "Grid view",
+            fields: ["Date"],
+            sort: [{ field: "Date", direction: "desc" }],
+        })
+        .firstPage();
+    const paths = posts.map(post => {
+        const { Date: date } = post.fields;
+        const [year, month, day] = date.split("-");
+        return {
+            params: {
+                year,
+                month,
+                day,
+            },
+        };
+    });
+    // console.log("getStaticPaths", "paths", paths);
     return {
-        paths: [{ params: { year: "2020", month: "03", day: "29" } }],
+        paths,
         fallback: false,
     };
 }
