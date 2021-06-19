0256657f6ce663043fcb0a351ddf0f9cbb8f349f
Michael Barrett <mb@abouthalf.com>
2021-06-19 10:35:20 -0700

    Rework MegaGram to support lazy loading in Safari

--- src/components/MegaGram/index.js
+++ src/components/MegaGram/index.js
@@ -1,6 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import slug from "slug";
 import PostHeader from "../PostHeader";
+
+import pixel from "../../lib/pixel";
+
+const ioConfig = {
+    rootMargin: '0px 0px 256px 0px',
+    threshold: 0.01,
+};
+const lazySrcAttr = "data-lazy-src";
+
+let intersectionObserver;
+
+const getIntersectionObserver = (onIntersection) => {
+    // create interaction observer
+    if (intersectionObserver) return intersectionObserver;
+    intersectionObserver = new IntersectionObserver(
+        onIntersection,
+        ioConfig
+    );
+    return intersectionObserver;
+}
 
 const MegaGram = ({
     index,
@@ -13,9 +33,11 @@ const MegaGram = ({
     path = "",
 }) => {
     if (!Array.isArray(index)) return null;
+    const hash = slug(date);
+    const postUrl = `/p/${year}/${month}/${day}`;
 
-    let hash = slug(date);
-    let postUrl = `/p/${year}/${month}/${day}`;
+    // get reverse array
+    const images = index.slice(0).reverse();
 
     return (
         <section id={hash} className="gram">
@@ -33,12 +55,6 @@ const MegaGram = ({
                     justify-content: center;
                     align-items: center;
                 }
-
-                .grid img {
-                    width: 33.33%;
-                    height: 33.33%;
-                    display: block;
-                }
             `}</style>
             <PostHeader
                 title={title}
@@ -47,18 +63,63 @@ const MegaGram = ({
                 date={date}
             />
             <div className="grid">
-                {index.reverse().map((img, i) => (
-                    <img
-                        key={img}
-                        width="900"
-                        height="900"
-                        src={`${path}${img}`}
-                        loading="lazy"
-                    />
-                ))}
+                {images.map((img, i) => {
+                    const src = `${path}${img}`;
+                    return <LazyImage key={slug(src + hash)} src={src} />
+                })}
             </div>
         </section>
     );
 };
 
 export default MegaGram;
+
+
+const LazyImage = ({ src }) => {
+    const ref = useRef();
+    // const [source, setSource] = useState(src);
+
+    // polyfill lazy loading for Safari
+    useEffect(() => {
+        // if lazy loading supported, use it
+        if ('loading' in HTMLImageElement.prototype) return;
+        // define onIntersection handler
+        function onIntersection(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio === 0) {
+                    return;
+                }
+                // If the item is visible now, load it and stop watching it
+                const image = entry.target;
+                observer.unobserve(image);
+                // load image
+                image.src = image.getAttribute(lazySrcAttr);
+            })
+        };
+
+        // observer current dom element, reset src to pre-load
+        if (ref?.current) {
+            const io = getIntersectionObserver(onIntersection);
+            io.observe(ref.current)
+            ref.current.src = pixel;
+        }
+    }, []);
+
+    return (<>
+        <style jsx>{`
+            img {
+                width: 33.33%;
+                height: 33.33%;
+                display: block;
+                will-change: contents;
+            }
+        `}</style>
+        <img
+            ref={ref}
+            width="900"
+            height="900"
+            src={src}
+            loading="lazy"
+            data-lazy-src={src}
+        /></>);
+}
